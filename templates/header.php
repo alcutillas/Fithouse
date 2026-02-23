fb66dcc56f2f4d691fda443b6148ca414a731f33
alcutillas <alvaromurada1@gmail.com>
2026-02-23 13:38:05 +0100

    Menu arreglado

--- templates/header.php
+++ templates/header.php
@@ -11,7 +11,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cabecera</title>
       <link rel="stylesheet" href="static/fontawesome/css/all.min.css">
-    <link rel="stylesheet" href="static/css/stylo.css">
+    <link rel="stylesheet" href="static/css/style.css">
 
     <?php
     if (isset($css)) {
@@ -42,58 +42,64 @@ session_start();
 </head>
 
 <body>
+<input type="checkbox" id="menu" hidden>
 
 <header>
-    <nav class="navbar">
-        
-        <div class="logo">
+<div class="logo">
             <a href="index.php" id="logo-menu">
                 <img src="static/img/logo-indice.png" width="55" height="40">
             </a>
         </div>
+<nav class="navbar">
+    <div class="menu-toggle" id="mobile-menu">
+        <i class="fa fa-bars"></i>
+    </div>
 
-        <!-- Botón hamburguesa -->
-            <input type="checkbox" name="check" id="check">
-            <label for="check"><i class="fa-solid fa-bars"></i></label>
-        
+    <ul class="nav-menu" id="nav-menu">
+        <?php if(isset($_SESSION["rol"]) && $_SESSION["rol"] === "admin"): ?>
+            <li class="admin-link"><a href="panel.php">Panel de control</a></li>
+        <?php endif; ?>
 
-        <ul class="nav-menu" id="nav-menu">
-
-            <?php if(isset($_SESSION["rol"]) && $_SESSION["rol"] === "admin"): ?>
-                <li><a href="panel.php">Panel de control</a></li>
-            <?php endif; ?>
-
-            <li><a href="acerca.php">Acerca de</a></li>
-
-            <li><a href="catalogo.php">Catálogo</a></li>
-
-            <li><a href="pedidos.php">Pedidos</a></li>
-
-            <li class="dropdown">
-                <input type="checkbox" name="check2" id="check2">
-                <label for="check2"><a href="#">Asesoramiento</a></label>
+        <li><a href="acercade.php">Acerca de</a></li>
+        <li class="dropdown">
+        <input type="checkbox" id="check2">
+                <label for="check2">Asesoramiento <i class="fa fa-caret-down"></i></label>
                 <ul class="submenu">
                     <li><a href="cuestionario.php">Cuestionario</a></li>
                 </ul>
+    </li>
+        <li><a href="catalogo.php">Catálogo</a></li>
+        <li><a href="pedidos.php">Pedidos</a></li>
+        <li><a href="contacto.php">Contacto</a></li>
+        
+        <?php if(empty($_SESSION)): ?>
+            <li class="dropdown user-options">
+                <input type="checkbox" id="check3">
+                <label for="check3"><i class="fa fa-user"></i> Cuenta <i class="fa fa-caret-down"></i></label>
+                <ul class="submenu">
+                    <li><a href="iniciarsesion.php">Iniciar Sesión</a></li>
+                    <li><a href="registrar.php">Registrarse</a></li>
+                </ul>
             </li>
-
-            <li><a href="contacto.php">Contacto</a></li>
-
-            <?php if(empty($_SESSION)): ?>
-                <li class="dropdown">
-                    <input type="checkbox" name="check3" id="check3">
-                    <label for="check3"></label><a href="#"><i class="fa fa-user"></i></a>
-                    <ul class="submenu">
-                        <li><a href="login.php">Iniciar Sesión</a></li>
-                        <li><a href="registro.php">Registrarse</a></li>
-                    </ul>
-                </li>
-            <?php else: ?>
-                <li><a href="logout.php">Cerrar Sesión</a></li>
-            <?php endif; ?>
-
-        </ul>
-    </nav>
+        <?php else: ?>
+            <li class="user-options"><a href="logout.php">Cerrar Sesión (<?php echo $_SESSION['usuario']; ?>)</a></li>
+        <?php endif; ?>
+        
+    </ul>
+</nav>
 </header>
+<script>
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById('mobile-menu');
+    const navMenu = document.getElementById('nav-menu');
 
-
+    menuToggle.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
+        
+        // Cambiar icono de hamburguesa a una X (opcional si usas FontAwesome)
+        const icon = menuToggle.querySelector('i');
+        icon.classList.toggle('fa-bars');
+        icon.classList.toggle('fa-times');
+    });
+});
+</script>
